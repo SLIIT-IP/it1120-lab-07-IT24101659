888105b3cb285255b1169878ab0caf656ce30992
IT24101659 <it24101659@my.sliit.lk>
2024-10-02 08:12:20 +0530

    add to Q2

---
+++ Q2/IT24101659Lab7Q2C.java
@@ -0,0 +1,23 @@
+public class IT24101659Lab7Q2C {
+    public static void main(String[] args) {
+
+        int i,j;
+        int k =5;
+
+   
+        for(i = 5; i>=1; i--) {
+
+            for(j = 1; j <= k; j++) {
+                System.out.print(i);
+                
+            }
+            k--;
+            System.out.print("\n");
+        
+        }
+
+
+
+    }
+}
+ 
